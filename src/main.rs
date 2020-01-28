e7315b2b339188f357b9be9e86df90cf07515b9d
Charles Inwald <cci219@lehigh.edu>
2020-01-28 16:39:02 -0500

    Can read config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+extern crate config;
+
+use structopt::StructOpt;
+use std::collections::HashMap;
+use config::Value;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    #[structopt(default_value = "foobar", long)]
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str), default_value = "config", long)]
+//    #[structopt(default_value = "foobar", String)]
+    path: std::path::PathBuf,
 }
+
+
+
+fn main() {
+    let args = Cli::from_args();
+    println!("Hello, world!");
+    println!("Pattern {}", &args.pattern);
+    println!("Path: {:?}", &args.path.display());
+
+    let mut settings = config::Config::default();
+    settings
+        // Add in `./Settings.toml`
+        .merge(config::File::with_name("Settings")).unwrap();
+
+    // Print out our settings (as a HashMap)
+    println!("{:?}",
+             settings.try_into::<HashMap<String, Vec<Value>>>().unwrap());
+}
+
