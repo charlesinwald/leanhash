bb1199e63aabb1b72e90505cb5662b78b0f7cffc
Charles Inwald <cci219@lehigh.edu>
2020-03-06 11:49:36 -0500

    working scripts

---
+++ stopnodes.sh
@@ -0,0 +1,3 @@
+ssh -t -i ~/Desktop/403Cluster.pem ec2-user@ec2-3-17-221-247.us-east-2.compute.amazonaws.com 'cd 403p1/403p1; ./stop.sh' 
+ssh -t -i ~/Desktop/403Cluster.pem ec2-user@ec2-13-58-219-209.us-east-2.compute.amazonaws.com 'cd 403p1; ./stop.sh'
+ssh -t -i ~/Desktop/403Cluster.pem ec2-user@ec2-3-12-195-112.us-east-2.compute.amazonaws.com 'cd 403p1; ./stop.sh' 
