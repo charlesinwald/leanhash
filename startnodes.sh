9b0bb8540b072748d9ee65d43b17ca488ffc8cbf
charlesinwald <cci219@lehigh.edu>
2020-12-28 20:53:58 -0500

    Delete startnodes.sh

--- startnodes.sh
+++
@@ -1,9 +0,0 @@
-docker image build -t 403p1 .
-docker run --net cluster --ip 172.18.0.2 403p1 &
-docker run --net cluster --ip 172.18.0.3 403p1 &
-docker run --net cluster --ip 172.18.0.4 403p1 &
-docker run --net cluster --ip 172.18.0.5 403p1 &
-docker run --net cluster --ip 172.18.0.6 403p1 &
-echo
-echo
-docker ps -a
